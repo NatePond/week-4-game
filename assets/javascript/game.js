e13982390997a1cb9357188355cd978b8a6b3cbf
Nate Pond <nathaniel.p.pond@gmail.com>
2017-03-14 00:50:38 -0400

    fifth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,98 @@
 //Crystal numbers are to be between 1 and 12 while the target number should be between 19 and 120  
 
-var targetNumber ;
+var targetNumber = Math.floor(Math.random() * 101) + 19;
 
-var aimNumber ;
 
-var crystal = {
-  red: 0,
-  orange: 0,
-  blue: 0,
-  green: 0
+var aimNumber = 0;
+
+var red = 0;
+var orange = 0;
+var blue = 0;
+var orange = 0;
+var addativeNumber = gem();
+
+
+var wins = 0;
+
+var losses = 0;
+
+var a;
+var b;
+var c;
+var d;
+
+
+function gem() {
+  return a = Math.floor(Math.random() * 11) + 1;
+  return b = Math.floor(Math.random() * 11) + 1;
+  return c = Math.floor(Math.random() * 11) + 1;
+  return d = Math.floor(Math.random() * 11) + 1;
+  if ( gem(a) === gem(b) || gem(a) === gem(c) || gem(a) === gem(d) || gem(b) === gem(c) || gem(b) === gem(d) || gem(c) === gem(d)) {
+    gem(); 
+  }
+}
+
+var isFixedValue = false;
+
+
+
+function setAllValues() {
+  if (isFixedValue === false) {
+    red = gem(a);
+    orange = gem(b);
+    blue = gem(c);
+    green = gem(d);
+  }
+  isFixedValue = true;
+  console.log(red);
+  console.log(orange);
+  console.log(blue);
+  console.log(green);
+}
+
+setAllValues();
+
+function controls(){
+$("#targetNumber").html(targetNumber)
+$(".red").on("click", function() {
+  aimNumber = aimNumber + red;
+  $("#aimNumber").html(aimNumber)
+});
+
+$(".orange").on("click", function() {
+  aimNumber = aimNumber + orange;
+  $("#aimNumber").html(aimNumber)
+});
+
+$(".blue").on("click", function() {
+  aimNumber = aimNumber + blue;
+  $("#aimNumber").html(aimNumber)
+});
+
+$(".green").on("click", function() {
+  aimNumber = aimNumber + green;
+  $("#aimNumber").html(aimNumber)
+})
 }
 
 
-function target(){
-  targetNumber =  Math.floor(Math.random()*101)+19;
-  $("#targetNumber").html(targetNumber);
-};
 
-target();
 
-function gemGenerator(crystal, orange){
-  for (key in crystal)
-  return randomNumber = Math.floor(Math.random()*11)+1;
-  if (gemGenerator(crystal.red) = gemGenerator(crystal.orange)){
-    gemGenerator(red, orange);
+function game() {
+  if (aimNumber === targetNumber) {
+    wins++;
+    targetNumber = Math.floor(Math.random() * 101) + 19;
+    $("#targetNumber").html(targetNumber)
   }
-  
+  else if (aimNumber > targetNumber) {
+    losses++;
+    targetNumber = Math.floor(Math.random() * 101) + 19;
+    $("#targetNumber").html(targetNumber)
+    return
   }
+  controls();
+  $("#wins").html(wins)
+  $("#losses").html(losses)
+}
 
-
-
-console.log("red " + gemGenerator(crystal.red));
-console.log("orange " + gemGenerator(crystal.orange));
-console.log("blue " + gemGenerator(crystal.blue));
-console.log("green " + gemGenerator(crystal.green));
-
- 
- 
- 
-gemGenerator();
-
+game();
