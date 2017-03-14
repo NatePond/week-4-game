c5d2d52caf68a357851076aa34d7121bba0f26fd
Nate Pond <nathaniel.p.pond@gmail.com>
2017-03-14 14:31:15 -0400

    sixth commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,81 +1,33 @@
-//Crystal numbers are to be between 1 and 12 while the target number should be between 19 and 120  
-
 var targetNumber = Math.floor(Math.random() * 101) + 19;
-
-
 var aimNumber = 0;
-
-var red = 0;
-var orange = 0;
-var blue = 0;
-var orange = 0;
-var addativeNumber = gem();
-
-
+var red;
+var orange;
+var blue;
+var orange;
 var wins = 0;
-
 var losses = 0;
-
-var a;
-var b;
-var c;
-var d;
-
+var addativeValue = []
 
 function gem() {
-  return Math.floor(Math.random() * 11) + 1;
-  // return a = Math.floor(Math.random() * 11) + 1;
-  // return b = Math.floor(Math.random() * 11) + 1;
-  // return c = Math.floor(Math.random() * 11) + 1;
-  // return d = Math.floor(Math.random() * 11) + 1;
-  // if ( gem(a) === gem(b) || gem(a) === gem(c) || gem(a) === gem(d) || gem(b) === gem(c) || gem(b) === gem(d) || gem(c) === gem(d)) {
-  //   gem(); 
-  // }
+  while (addativeValue.length < 5) {
+    var randomnumber = Math.floor(Math.random() * 11) + 1;
+    if (addativeValue.indexOf(randomnumber) > -1) continue;
+    return addativeValue[addativeValue.length] = randomnumber;
+  }
 }
 
-// var isFixedValue = false;
-
-
-
 function setAllValues() {
-  // if (isFixedValue === false) {
-    red = gem();
-    orange = gem();
-    blue = gem();
-    green = gem();
-  // }
-  isFixedValue = true;
+  red = gem(addativeValue[0]);
+  orange = gem(addativeValue[1]);
+  blue = gem(addativeValue[2]);
+  green = gem(addativeValue[3]);
   console.log(red);
   console.log(orange);
   console.log(blue);
   console.log(green);
 }
-
 setAllValues();
 
-// $("#targetNumber").html(targetNumber)
-// $(".red").on("click", function() {
-//   aimNumber = aimNumber + red;
-//   $("#aimNumber").html(aimNumber)
-// });
-
-// $(".orange").on("click", function() {
-//   aimNumber = aimNumber + orange;
-//   $("#aimNumber").html(aimNumber)
-// });
-
-// $(".blue").on("click", function() {
-//   aimNumber = aimNumber + blue;
-//   $("#aimNumber").html(aimNumber)
-// });
-
-// $(".green").on("click", function() {
-//   aimNumber = aimNumber + green;
-//   $("#aimNumber").html(aimNumber)
-// })
-
-
-// $("#targetNumber").html(10)
 function game() {
 
   $("#targetNumber").html(targetNumber)
@@ -84,12 +36,18 @@ function game() {
     $("#aimNumber").html(aimNumber)
     if (aimNumber === targetNumber) {
       wins++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       aimNumber = 0;
       $("#targetNumber").html(targetNumber);
       $("#aimNumber").html(aimNumber);
     } else if (aimNumber > targetNumber) {
       losses++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       aimNumber = 0;
       $("#targetNumber").html(targetNumber)
@@ -104,11 +62,17 @@ function game() {
     $("#aimNumber").html(aimNumber)
     if (aimNumber === targetNumber) {
       wins++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       $("#targetNumber").html(targetNumber);
       $("#aimNumber").html(0);
     } else if (aimNumber > targetNumber) {
       losses++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       aimNumber = 0;
       $("#targetNumber").html(targetNumber)
@@ -121,12 +85,18 @@ function game() {
     $("#aimNumber").html(aimNumber)
     if (aimNumber === targetNumber) {
       wins++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       aimNumber = 0;
       $("#targetNumber").html(targetNumber);
       $("#aimNumber").html(aimNumber);
     } else if (aimNumber > targetNumber) {
       losses++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       aimNumber = 0;
       $("#targetNumber").html(targetNumber)
@@ -139,6 +109,9 @@ function game() {
     $("#aimNumber").html(aimNumber)
     if (aimNumber === targetNumber) {
       wins++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       aimNumber = 0;
       $("#targetNumber").html(targetNumber);
@@ -146,13 +119,16 @@ function game() {
       $("#aimNumber").html(aimNumber);
     } else if (aimNumber > targetNumber) {
       losses++;
+      addativeValue = [];
+      gem();
+      setAllValues();
       targetNumber = Math.floor(Math.random() * 101) + 19;
       aimNumber = 0;
       $("#targetNumber").html(targetNumber)
       $("#aimNumber").html(aimNumber);
     }
   })
-  
+
 }
 
 game();
